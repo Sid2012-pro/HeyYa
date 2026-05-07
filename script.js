365b3d2c6782409c87fd9333c362f9e0cd8cd8e0
Sid2012-pro <sidsassy28@gmail.com>
2026-05-07 17:10:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const quoteElement = document.getElementById('quote');
+const audio = document.getElementById('audioPlayer');
+let currentSongNum = 1;
+const totalSongs = 6;
+audio.src = "1.mp3";
+audio.loop = true;
+const quotes = [
+    "Everything is going to be alright! The <span class='highlight'>luck</span> is on your side!",
+    "Don't look back, just keep moving forward!",
+    "The shortest route was a detour. You're doing great!",
+    "Trust your instincts! You've got this!",
+    "You are the only one who can make it happen!",
+    "Keep that chin up! You're in for a lucky break!",
+    "Whatever happens, it's all part of your success story!"
+];
+function displayNewQuote() {
+    const randomNum = Math.floor(Math.random() * quotes.length);
+    quoteElement.innerHTML = quotes[randomNum];
+}
+function updateSong() {
+    audio.src = `${currentSongNum}.mp3`;
+    audio.play().catch(error => {
+        console.log("Waiting for user interaction to play audio");
+    });
+}
+function nextSong() {
+    if (currentSongNum < totalSongs) {
+        currentSongNum++;
+    } else {
+        currentSongNum = 1;
+    }
+    updateSong();
+}
+function prevSong() {
+    if (currentSongNum > 1) {
+        currentSongNum--;
+    } else {
+        currentSongNum = totalSongs;
+    }
+    updateSong();
+}
+function reload() {
+    displayNewQuote();
+    if (audio.paused) {
+        audio.play().catch(e => console.log("Play failed"));
+    }
+}
+
+window.onload = displayNewQuote;
